0954eb061286df681b1b9b7d50185d955c4a85a3
Dionis <denis.shastin@gmail.com>
2021-05-13 17:24:11 +0300

    course3 week1 done

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', TemplateView.as_view(template_name='home/main.html')),
     path('owner/', views.owner, name='owner'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
