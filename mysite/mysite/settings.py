f7dc3d103a2a0ff85b2a36281d7f7c99d7fcccae
Dionis <denis.shastin@gmail.com>
2021-05-13 17:24:11 +0300

    course3 week1 done

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hello',
+    'home',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
